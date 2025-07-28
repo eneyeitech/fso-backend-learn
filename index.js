c68b41d801a754b206de411b13e12b7461aaf8ed
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2025-07-28 10:54:15 +0100

    rest client

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: "1",
@@ -27,6 +29,33 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find(note => note.id === id)
@@ -45,6 +74,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
